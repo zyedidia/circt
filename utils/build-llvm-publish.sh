996066b8dbb69e9391caa38ec1b859779de4dac5
Zachary Yedidia <zyedidia@gmail.com>
2022-07-25 16:00:48 -0700

    Update

--- utils/build-llvm-publish.sh
+++ utils/build-llvm-publish.sh
@@ -13,26 +13,22 @@
 ##===----------------------------------------------------------------------===##
 
 BUILD_DIR=${1:-"build"}
-INSTALL_DIR=${2:-"install"}
-BUILD_TYPE=${3:-"Release"}
-CC=${4:-"clang"}
-CXX=${5:-"clang++"}
-EXTRA_ARGS=${@:6}
+BUILD_TYPE=${2:-"Release"}
+CC=${3:-"clang"}
+CXX=${4:-"clang++"}
+EXTRA_ARGS=${@:5}
 
 mkdir -p llvm/$BUILD_DIR
-mkdir -p llvm/$INSTALL_DIR
 cd llvm/$BUILD_DIR
 cmake -G Ninja ../llvm \
     -DCMAKE_BUILD_TYPE=$BUILD_TYPE \
     -DCMAKE_C_COMPILER=$CC \
     -DCMAKE_CXX_COMPILER=$CXX \
-    -DCMAKE_INSTALL_PREFIX=../$INSTALL_DIR \
     -DLLVM_BUILD_EXAMPLES=OFF \
     -DLLVM_ENABLE_ASSERTIONS=OFF \
     -DLLVM_ENABLE_BINDINGS=OFF \
     -DLLVM_ENABLE_OCAMLDOC=OFF \
     -DLLVM_ENABLE_PROJECTS='mlir' \
-    -DLLVM_INSTALL_UTILS=ON \
     -DLLVM_OPTIMIZED_TABLEGEN=ON \
     -DLLVM_STATIC_LINK_CXX_STDLIB=ON \
     -DLLVM_ENABLE_TERMINFO=OFF \
